f848d3cc0f014b74dee3ef78296f0fec4ba13510
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-10-10 12:41:59 +0200

    created project

---
+++ src/main/java/com/greenfoxacademy/zerothweekproject/ZerothweekprojectApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.zerothweekproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerothweekprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerothweekprojectApplication.class, args);
+    }
+}
